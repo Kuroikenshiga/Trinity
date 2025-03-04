4e5fc0db1f71750045c70bbdbe84623aecc70941
Kuroikenshiga <juniorsilvapalafoz@gmail.com>
2025-03-04 16:53:52 -0300

    fix: updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 1
-        versionName = "2.4.3"
+        versionName = "2.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
